0b568cb93d0bdd82921f616ab342c3747c426332
SnehalNakade <snehalnakade98@gmail.com>
2022-04-02 11:48:50 +0530

    Added: Seven

---
+++ Seven/SumOfThree.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for (( i=0;i<3;i++ ))
+do
+        read -p "Enter the 3 Number: " Num
+                Array[i]=$Num
+done
+echo Array : [ ${Array[@]} ]
+Result=0
+for (( i=0;i<3;i++ ))
+do
+    Result=$(( $Result+${Array[i]} ))
+done
+if (( $Result==0 ))
+then
+        echo "Sum is :$Result"
+else
+        echo "Sum is :$Result"
+fi
+
